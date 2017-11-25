7245acf8788b7cd4abeceb9287ed37f4ee2c9c53
jsrj <arjay.jones3301@gmail.com>
2017-11-25 00:56:03 -0500

    switch technologies for client side

--- buildershare-api/routes/index.js
+++ buildershare-api/routes/index.js
@@ -5,7 +5,7 @@ const router  = express.Router();
 router.get('/', (req, res, next) => {
   //res.render('index');
   res.json({
-    "message": "welcome"
+    "message": "welcome! Something not working the way it should? Let us know, and earn credit toward your account for reporting the inconvenience."
   });
 });
 
